08eb9aa5689bce2fd96f5231c29d9fab56d51744
dimka90 <dimkayilrit@gmail.com>
2024-03-15 14:24:23 +0100

    added a function that concate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+A functiont that concate two strings
+"""
+
+def concat(str1: str, str2:str) -> str:
+    """
+    Concate two strings
+
+    Argument:
+            str1: First string 
+            str2: Second string
+    Return:
+           str: Returns the concantenated string
+    """
+
+    return str1 + str2
