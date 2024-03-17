7664d21831a2e40ce67be5784fa38b3d922318bd
dimka90 <dimkayilrit@gmail.com>
2024-03-17 06:12:35 +0100

    created a function that convert a float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+This programs converts a float to string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This functions cast a floor to  a str.
+    Argument(s):
+                n: a floating number to converted to a string
+    Return:
+          A string
+    """
+    return str(n)
