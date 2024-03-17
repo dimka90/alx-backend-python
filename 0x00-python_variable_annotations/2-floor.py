1ab3e67059755850fdea20a854ffb2edce45149e
dimka90 <dimkayilrit@gmail.com>
2024-03-17 05:53:09 +0100

    created a function in a module that takes in a float number and convert it to a int using the flooor(//) symbol

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+This program takes in a float floor it.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    A function that takes in a float and cast it to a int.
+
+    Arguments:
+            n: A float point number to be converted to an int
+    Returns:
+            int
+    """
+
+    return 3 // 1
