a7f0bf8de553783165f6cc273e54d4063a70e128
dimka90 <dimkayilrit@gmail.com>
2024-03-17 06:21:31 +0100

    created a python module that uses annotation to define vairables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+A program that declares vairable using annotations
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
