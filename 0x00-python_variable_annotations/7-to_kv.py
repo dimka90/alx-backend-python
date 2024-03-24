8fb847662c6cf9cb29256e2d833d3cb996a4fa50
dimka90 <dimkayilrit@gmail.com>
2024-03-24 12:15:06 +0100

    created a function that takes in a list containing a string and either a float or an int and returns a tuple containning the str and the number raised to the exponent of 2

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+to_kv that takes a string k and an int OR float
+v as arguments and returns a tuple. The first
+element of the tuple is the string k. The second
+element is the square of the int/floatv and should
+be annotated as a float.
+"""
+
+from typing import List, Union
+
+
+def to_kv(k: str, v: List[Union[int, float]]) -> tuple[str, float]:
+    """
+    A function that takes a str and either a float or an int
+    and return the str, raising the int / float to by exponent
+    of two
+    Arguments:
+            k: String
+            v: int/float to raise to exponent of 2
+    Returns:
+            Tuple containing a str and the result of the int/float by
+            raised to the power of two
+    """
+
+    return (k, v ** 2)
